656c5919cd7cf2073f090f19a9abd7a13f70e669
USTBJJT <JJT_famwaf67@outlook.com>
2017-03-06 22:46:08 +0800

    fix java file

---
+++ Git_proj/src/ustb/edu/cn/BranchTest.java
@@ -0,0 +1,32 @@
+package ustb.edu.cn;
+
+public class BranchTest {
+	
+	private String user_name = null;
+	private String pass_word = null;
+	
+	public String getName() {
+		return user_name;
+	}
+	
+	public String getPW() {
+		return pass_word;
+	}
+	
+	public void setName(String user_name) {
+		this.user_name = user_name;
+	}
+	
+	public void setPW(String pass_word) {
+		this.pass_word = pass_word;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BranchTest bt = new BranchTest();
+		bt.setName("USTB");
+		System.out.println(bt.getName());
+		
+	}
+
+}
